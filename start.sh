0108e53b6cb4bad21f71dff2a7dc9f894ec50c6e
pradit07 <76438201+pradit07@users.noreply.github.com>
2022-07-21 12:28:39 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ if [ ! -f /usr/share/nginx/html/conf/config.js ]; then
 fi
 
 if [ $API_URL ]; then
-  echo "当前 API 地址为: $API_URL"
-  sed -i "s#http://127.0.0.1:25500#$API_URL#g" /usr/share/nginx/html/conf/config.js
+  echo "Bawaan: $API_URL"
+  sed -i "s#http://sub.bonds.id#$API_URL#g" /usr/share/nginx/html/conf/config.js
 else
   echo "Bawaan: https://martoleh.vercel.app/"
   echo "Manual -e API_URL='https://sub.ops.ci'"
