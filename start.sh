e0b706514b7a5cd9b08adf05c48ce3f1ef942ce3
pradit07 <76438201+pradit07@users.noreply.github.com>
2022-07-21 12:14:50 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,8 +7,8 @@ if [ $API_URL ]; then
   echo "当前 API 地址为: $API_URL"
   sed -i "s#http://127.0.0.1:25500#$API_URL#g" /usr/share/nginx/html/conf/config.js
 else
-  echo "当前为默认本地 API 地址: http://127.0.0.1:25500"
-  echo "如需修改请在容器启动时使用 -e API_URL='https://sub.ops.ci' 传递环境变量"
+  echo "Bawaan: https://martoleh.vercel.app/"
+  echo "Manual -e API_URL='https://sub.ops.ci'"
 fi
 
 if [ $SITE_NAME ]; then
